3acefddf82603781808a740b089301c1862edf67
Yuto_0417 <kn.rain.0147@gmail.com>
2022-02-16 09:33:40 +0900

    reb

--- OpenSiv3D_0.6.31_marioLike/Game.cpp
+++ OpenSiv3D_0.6.31_marioLike/Game.cpp
@@ -2,63 +2,128 @@
 #include "Game.h"
 
 
-
-
 Game::Game(const InitData& init):IScene(init)
 {
 	//visitor.inspector[Accessor::first] = [&]() {return foo(); };
 	//std::visit(visitor, visitor.inspector.at(visitor.stateMachine.front()));
-	player.LVbody() = world.createRect(P2Dynamic, Vec2(0, -100), RectF(CHIP_SIZE, CHIP_SIZE * 2), P2Material());
+	player.LVRbody() = world.createRect(P2Dynamic, Vec2(0, -100), RectF(Arg::center(CHIP_SIZE,CHIP_SIZE),CHIP_SIZE, CHIP_SIZE * 2), P2Material(1.0,1.0,0.2,0.0));
+	//player.LVRbody().addRect(RectF(30, 100));
+
+	player.LVRbody().addTriangle(Triangle(32+16,0,42+16,32,32+16,64), P2Material(1.0, 1.0, 0.2, 0.0));
+	player.LVRbody().addTriangle(Triangle(16,0,4,32,16,64), P2Material(1.0, 1.0, 0.2, 0.0));
+
+	player.LVRbody().setDamping(0.1);
+	player.LVRbody().setFixedRotation(true);
+	//RectF rr(Arg::center(CHIP_SIZE,CHIP_SIZE * 2), CHIP_SIZE, 32 / 2);
+	//player.LVRbody().addRect(rr);
 	Size size = { map.width(), map.height() };//マップの大きさ｛ｘ、ｙ｝
 	for (auto p : step(size)) {
 		if (map[p.y][p.x] == 1)
 			chips << world.createRect(P2Static, Vec2{ p.x * 32 - 1280 / 2,p.y * 32 - 720 / 2 }, RectF(32, 32, 32, 32), P2Material());
 	}
-	camera.setParameters(Camera2DParameters::MouseOnly());
+	for (auto p : step(Size(enemy.width(),enemy.height()))) {
+		if (enemy[p.y][p.x] == 1)
+			enemys << world.createRect(P2Dynamic, Vec2{ p.x * 32 - 1280 / 2,p.y * 32 - 720 / 2 }, RectF(32, 32, 32, 32), P2Material());
+	}
+	camera.setParameters(Camera2DParameters::NoControl());
 }
+
+
+
 
 void Game::update()
 {
-
-	
+	//ClearPrint();
 	for (accumulatorSec += Scene::DeltaTime(); stepSec <= accumulatorSec; accumulatorSec -= stepSec)
 	{
-		// 2D 物理演算のワールドを更新
 		world.update(stepSec);
+
+		PrintDebug();
+		for (auto& it : world.getCollisions())
+		{
+			if (it.second.normal() == Vec2(0, 1)
+				and (KeySpace.pressed() or KeySpace.down())
+				and not player.isJumpRestriction())
+			{
+				player.LVRbody().applyLinearImpulse(Vec2(0, player.param().JUMP_POWER));
+				player.LVRisJumpRestriction() = true;
+			}
+			if (it.second.normal() == Vec2(1, 0)
+				and (KeyD.pressed() or KeyRight.pressed()))
+			{
+				player.LVRbody().setVelocity(Vec2(0, 0));
+				if (KeySpace.down())
+				{
+					player.LVRbody().applyLinearImpulse(Vec2(-500, -100));
+				}
+			}
+			if (it.second.normal() == Vec2(1, 0)
+				and (KeyD.up() or KeyRight.up()))
+			{
+				player.LVRbody().applyLinearImpulse(Vec2(-1000, 0));
+			}
+			if (it.second.normal() == Vec2(-1, 0)
+				and (KeyA.pressed() or KeyLeft.pressed()))
+			{
+				player.LVRbody().setVelocity(Vec2(0, 0));
+				if (KeySpace.down())
+				{
+					player.LVRbody().applyLinearImpulse(Vec2(500, -100));
+				}
+			}
+			if (it.second.normal() == Vec2(-1, 0)
+				and (KeyA.up() or KeyLeft.up()))
+			{
+				player.LVRbody().applyLinearImpulse(Vec2(1000, 0));
+			}
+		}
+		
 	}
+	if (KeySpace.up())
+	{
+		player.LVRisJumpRestriction() = false;
+	}
+
+	if (KeyA.pressed() or KeyLeft.pressed())
+	{
+		player.LVRbody().applyForce(Vec2(-player.param().ACCELERATION_X, 0));
+	}
+	if (KeyD.pressed() or KeyRight.pressed())
+	{
+		player.LVRbody().applyForce(Vec2(player.param().ACCELERATION_X, 0));
+	}
+	double vel = Clamp(player.body().getVelocity().x, -player.param().MAX_VELOCITY_X, player.param().MAX_VELOCITY_X);
+	player.LVRbody().setVelocity(Vec2(vel, player.body().getVelocity().y));
 
 	camera.update();
 	camera.setCenter(player.body().getPos());
-	//{
-
-	//	// 2D カメラの設定から Transformer2D を作成
-	//	const auto t = camera.createTransformer();
-
-	//	for (int32 i = 0; i < 8; ++i)
-	//	{
-	//		Circle{ 0, 0, (50 + i * 50) }.drawFrame(2);
-	//	}
-
-	//	Shape2D::Star(50, Vec2{ 200, 200 }).draw(Palette::Yellow);
-	//}
-
-	//camera.update();
-	//{
-	//	const auto t = camera.createTransformer();
-	//	for (const auto& chip : chips)
-	//	{
-	//		chip.draw(HSV{ chip.id() * 10.0 });
-	//	}
-	//}
+	//camera.setScale(0.5);
+	
+	//PrintDebug();
 }
 
 void Game::draw() const
 {
+	//TextureAsset(U"sampleBack").scaled(1.5,1.5).draw();
 	const auto t = camera.createTransformer();
-	player.body().draw();
+	player.body().drawWireframe();
 	for (const auto& chip : chips)
 	{
 		chip.draw(HSV{ chip.id() * 10.0 });
 	}
+	for (const auto& enemy : enemys)
+	{
+		enemy.draw(HSV{ enemy.id() * 10.0 });
+	}
 	
+}
+
+void Game::PrintDebug()
+{
+	for (auto& it : world.getCollisions())
+	{
+		Print << it.first.a;
+		Print << it.first.b;
+		Print << it.second.normal();
+	}
 }
